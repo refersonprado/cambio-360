08d0edba56fd57e8f37d36fd2ff9b54dfb294105
refersonprado <prado.referson@gmail.com>
2024-08-24 19:50:16 -0300

    feat: create currency rate interface

---
+++ src/app/interfaces/currency-rate.ts
@@ -0,0 +1,12 @@
+export interface CurrencyRate {
+    code: string;
+    codein: string;
+    name: string;
+    high: string;
+    low: string;
+    varBid: string;
+    pctChange: string;
+    bid: string;
+    ask: string;
+    timestamp: string;
+}
